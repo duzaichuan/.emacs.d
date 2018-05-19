b91b24c9ee69aa49137ed44e740996029309890b
duzaichuan <duzaichuan.qq.com>
2018-05-20 00:45:36 +0200

    eshell fix

--- lisp/du-toolkit.el
+++ lisp/du-toolkit.el
@@ -74,6 +74,7 @@
    (ivy-set-occur 'ivy-switch-buffer 'ivy-switch-buffer-occur)))
 
 (use-package swiper
+  :ensure t
   :bind ("\C-s" . swiper)
   :config
   (defun haba/swiper-mc-fixed ()
@@ -150,15 +151,109 @@
     ))
 
 (use-package undo-tree
+  :ensure t
   :config
   (global-undo-tree-mode))
 
 (use-package magit
   :ensure t
-  :bind ("C-x g" . magit-status))
+  :bind ("C-x g" . magit-status)
+  )
 
 (use-package projectile
   :ensure t
   :bind ([f5] . projectile-compile-project))
+
+(use-package eshell
+  :ensure t
+  :bind ([f1] . eshell)
+  :commands eshell-mode
+  :init
+  (add-hook 'eshell-mode-hook
+            (lambda ()
+              (eshell/export "NODE_NO_READLINE=1")))
+  ;; remove linum
+  (add-hook 'eshell-mode-hook (lambda () (linum-mode -1)))
+  :config
+  (progn
+    (require 'magit)
+    (defmacro with-face (STR &rest PROPS)
+      "Return STR propertized with PROPS."
+      `(propertize ,STR 'face (list ,@PROPS)))
+
+    (defmacro esh-section (NAME ICON FORM &rest PROPS)
+      "Build eshell section NAME with ICON prepended to evaled FORM with PROPS."
+      `(setq ,NAME
+             (lambda () (when ,FORM
+			  (-> ,ICON
+			      (concat esh-section-delim ,FORM)
+			      (with-face ,@PROPS))))
+	     ))
+
+    (defun esh-acc (acc x)
+      "Accumulator for evaluating and concatenating esh-sections."
+      (--if-let (funcall x)
+	  (if (s-blank? acc)
+              it
+            (concat acc esh-sep it))
+	acc))
+
+    (defun esh-prompt-func ()
+      "Build `eshell-prompt-function'"
+      (concat esh-header
+              (-reduce-from 'esh-acc "" eshell-funcs)
+              "\n"
+              eshell-prompt-string))
+
+    (esh-section esh-dir
+		 "\xf07c"  ;  (faicon folder)
+		 (abbreviate-file-name (eshell/pwd))
+		 '(:foreground "black" :bold ultra-bold :underline t))
+
+    (esh-section esh-git
+		 "\xe907"  ;  (git icon)
+		 (magit-get-current-branch)
+		 '(:foreground "grey40"))
+
+    ;; Below I implement a "prompt number" section
+    (setq esh-prompt-num 0)
+    (add-hook 'eshell-exit-hook (lambda () (setq esh-prompt-num 0)))
+    (advice-add 'eshell-send-input :before
+		(lambda (&rest args) (setq esh-prompt-num (incf esh-prompt-num))))
+
+    (esh-section esh-num
+		 "\xf0c9"  ;  (list icon)
+		 (number-to-string esh-prompt-num)
+		 '(:foreground "grey60"))
+
+    ;; Separator between esh-sections
+    (setq esh-sep "  ")  ; or " | "
+
+    ;; Separator between an esh-section icon and form
+    (setq esh-section-delim " ")
+
+    ;; Eshell prompt header
+    (setq esh-header " ")  ; or "\n┌─"
+
+    ;; Eshell prompt regexp and string. Unless you are varying the prompt by eg.
+    ;; your login, these can be the same.
+    (setq eshell-prompt-regexp "$ ")   ; or "└─> "
+    (setq eshell-prompt-string "$ ")   ; or "└─> "
+
+    ;; Choose which eshell-funcs to enable
+    (setq eshell-funcs (list esh-dir esh-git esh-num))
+
+    ;; Enable the new eshell prompt
+    (setq eshell-prompt-function 'esh-prompt-func)
+
+  ))
+
+(use-package eshell-fringe-status
+  :ensure t
+  :hook (eshell-mode . eshell-fringe-status-mode))
+
+(use-package esh-autosuggest
+  :ensure t
+  :hook (eshell-mode . esh-autosuggest-mode))
 
 (provide 'du-toolkit)
