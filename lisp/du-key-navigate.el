885bafe7bf36441c7955e470a899afedff52197a
duzaichuan <duzaichuan.qq.com>
2018-06-19 16:19:29 +0200

    evil fix2

--- lisp/du-key-navigate.el
+++ lisp/du-key-navigate.el
@@ -13,59 +13,73 @@
 (use-package evil
   :ensure t
   :diminish evil-mode
-  :init (setq evil-want-integration nil)
+  :init
+  (progn
+    (setq evil-want-integration nil) 
+    (use-package evil-leader
+      :ensure t
+      :init (global-evil-leader-mode)
+      :config
+      (progn
+	(evil-leader/set-leader "SPC")
+	(evil-leader/set-key "wd" 'delete-window)
+	(evil-leader/set-key "wo" 'delete-other-windows)
+	(evil-leader/set-key "wh" 'split-window-horizontally)
+	(evil-leader/set-key "wv" 'split-window-vertically)
+	(evil-leader/set-key "ww" 'other-window)
+	(evil-leader/set-key "bb" 'ivy-switch-buffer)
+	(evil-leader/set-key "bq" 'kill-buffer)
+	(evil-leader/set-key "bj" 'next-buffer)
+	(evil-leader/set-key "bk" 'previous-buffer)
+	(evil-leader/set-key ":" 'counsel-M-x)
+	(evil-leader/set-key "ff" 'counsel-find-file)
+	(evil-leader/set-key "fr" 'recentf-open-files)
+	(evil-leader/set-key "gs" 'magit-status)
+	(evil-leader/set-key "gu" 'browse-url-at-point)	
+	(evil-leader/set-key "cc" 'evilnc-comment-or-uncomment-lines)
+	(evil-leader/set-key "oc" 'org-capture)
+	(evil-leader/set-key "oa" 'org-agenda)
+	(evil-leader/set-key "ob" 'org-iswitchb)
+	(evil-leader/set-key "or" 'org-refile)
+	(evil-leader/set-key "ol" 'org-store-link)
+	(evil-leader/set-key "oe" 'org-export-dispatch)
+	(evil-leader/set-key "of" 'writeroom-mode)
+	(evil-leader/set-key "ok" 'org-priority-up)
+	(evil-leader/set-key "oj" 'org-priority-down)
+	(evil-leader/set-key "ot" 'org-time-stamp)
+	(evil-leader/set-key "dd" 'osx-dictionary-search-word-at-point)
+	(evil-leader/set-key "di" 'osx-dictionary-search-input)
+	(evil-leader/set-key "ej" 'emms-next)
+	(evil-leader/set-key "ek" 'emms-previous)
+	(evil-leader/set-key "ep" 'emms-pause) )) )
   :config
   (progn
     (evil-mode 1)
     (setq evil-cross-lines t)
     (setq evil-move-cursor-back nil)
+    (setq evil-want-C-u-scroll t)
     (setcdr evil-insert-state-map nil)
     (define-key evil-insert-state-map [escape] 'evil-normal-state)
-    (setq evil-want-C-u-scroll t)
     (evil-add-hjkl-bindings recentf-dialog-mode-map 'emacs)
     (evil-add-hjkl-bindings package-menu-mode-map 'emacs)
     (evil-add-hjkl-bindings pdf-outline-buffer-mode-map 'emacs)
     (evil-add-hjkl-bindings osx-dictionary-mode-map 'emacs)
     (evil-add-hjkl-bindings emms-playlist-mode-map 'emacs)
-    (evil-set-initial-state 'messages-buffer-mode 'emacs) ))
-
-(use-package evil-leader
-  :ensure t
-  :after evil
-  :init (global-evil-leader-mode)
-  :config
-  (progn
-    (evil-leader/set-leader "SPC")
-    (evil-leader/set-key "wd" 'delete-window)
-    (evil-leader/set-key "wo" 'delete-other-windows)
-    (evil-leader/set-key "wh" 'split-window-horizontally)
-    (evil-leader/set-key "wv" 'split-window-vertically)
-    (evil-leader/set-key "ww" 'other-window)
-    (evil-leader/set-key "bb" 'ivy-switch-buffer)
-    (evil-leader/set-key "bk" 'kill-buffer)
-    (evil-leader/set-key ":" 'counsel-M-x)
-    (evil-leader/set-key "ff" 'counsel-find-file)
-    (evil-leader/set-key "fr" 'recentf-open-files)
-    (evil-leader/set-key "gs" 'magit-status)
-    (evil-leader/set-key "gu" 'browse-url-at-point)
-    (evil-leader/set-key "cc" 'evilnc-comment-or-uncomment-lines)
-    (evil-leader/set-key "oc" 'org-capture)
-    (evil-leader/set-key "oa" 'org-agenda)
-    (evil-leader/set-key "or" 'org-refile)
-    (evil-leader/set-key "ol" 'org-store-link)
-    (evil-leader/set-key "oe" 'org-export-dispatch)
-    (evil-leader/set-key "of" 'writeroom-mode)
-    (evil-leader/set-key "dd" 'osx-dictionary-search-word-at-point)
-    (evil-leader/set-key "di" 'osx-dictionary-search-input)
-    (evil-leader/set-key "ej" 'emms-next)
-    (evil-leader/set-key "ek" 'emms-previous)
-    (evil-leader/set-key "ep" 'emms-pause) ))
+    
+    (defun my-evil-record-macro ()
+      (interactive)
+      (if buffer-read-only
+	  (quit-window)
+	(call-interactively 'evil-record-macro)))
+    ;; evil quit-window and evil-record-macro integration
+    (with-eval-after-load 'evil-maps
+      (define-key evil-normal-state-map (kbd "q") 'my-evil-record-macro)) ))
 
 (use-package evil-collection
   :after evil
   :ensure t
   :config
-  (evil-collection-init '(eww eshell dired)))
+  (evil-collection-init '(eww eshell cider company dired package-menu)))
 
 (use-package evil-magit
         :ensure t
