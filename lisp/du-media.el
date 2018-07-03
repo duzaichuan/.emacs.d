ed9035a46d75c5c27e1848194a2bc8dce407c5d4
duzaichuan <duzaichuan.qq.com>
2018-07-03 16:16:11 +0200

    add circe

--- lisp/du-media.el
+++ lisp/du-media.el
@@ -18,4 +18,13 @@
 	      twittering-icon-mode t
 	      twittering-use-icon-storage t))
 
+(use-package circe
+  :ensure t
+  :commands circe
+  :init (setq circe-network-options
+	      '(("127.0.0.1"
+		 :port "6667"
+		 :tls t
+		 :nick "Z.Du"))))
+
 (provide 'du-media)
